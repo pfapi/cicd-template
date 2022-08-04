342ac137e4693e61243175275fec04c581b6b284
Sam Wen <samw@ritani.com>
2022-08-04 10:44:33 -0400

    update

--- packages/three/src/index.js
+++ packages/three/src/index.js
@@ -5,9 +5,9 @@ const two = require('@pfapi/cicd-test-two');
 
 module.exports = () => {
 
-    console.log('*** three is running! ***');
+    console.log('***  three is running!  ***');
     console.log('call one', one());
     console.log('call two', two());
-    
+
     return 'three';
 }
